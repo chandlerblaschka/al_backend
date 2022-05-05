baf0bbaeea0138af95394e3fdc4ea1c40805a058
chandlerblaschka <cblaschka@live.com>
2022-05-05 17:01:46 -0500

    cascade

--- activityHost/settings.py
+++ activityHost/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-4e%dayr&cp++bh7cahb@umwc@hb-*ecex-p27jacch0a25@qcn'
 
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-4e%dayr&cp++bh7cahb@umwc@hb-*ecex-p27jacch0a25@qcn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
